05376cfc147e21463f08ff584067a287738191be
pedromain <macieirapf@gmail.com>
2025-04-20 22:52:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    const messageElement = document.getElementById('specialMessage');
+    messageElement.textContent = "Luana, você é a razão do meu sorriso! 💕";
+    messageElement.style.color = '#d741a3';
+    messageElement.style.fontSize = '1.5em';
+    messageElement.style.fontWeight = 'bold';
+}
